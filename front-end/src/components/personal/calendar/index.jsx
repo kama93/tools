2018970531b2be5a51884527c984dcb14baeff07
Kamila Burzynska <kamilaszabela@gmail.com>
2024-05-01 20:23:58 +0100

    update popup with ant design one, adding time picker and new input style

--- front-end/src/components/personal/calendar/index.jsx
+++ front-end/src/components/personal/calendar/index.jsx
@@ -1,19 +1,24 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {CloseOutlined} from '@ant-design/icons';
 
 import dayjs from 'dayjs';
 
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import ListGroup from 'react-bootstrap/ListGroup';
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+// import Form from 'react-bootstrap/Form';
+// import ListGroup from 'react-bootstrap/ListGroup';
+
+import { Modal, TimePicker } from 'antd';
+import moment from 'moment';
 
 
-import { Calendar } from "antd";
+import {Calendar} from "antd";
 
 import './style.css';
 
-// zrobic popover z chmurka, moze uzyc timeline
+// moze uzyc timeline
+// zeby server zapisywal godzine i nadpisywal jesli ta sama godzina- ale musi tez dawac komunikat jesli juz zajeta godzina
+
 
 function CalendarComponent () {
     const [value, setValue] = useState(() => dayjs(new Date().toJSON().slice(0, 10)));
@@ -22,9 +27,10 @@ function CalendarComponent () {
     let [currentEvents, setCurrentEvents] = useState({});
 
     const inputRef = useRef(null);
+    const format = 'HH:mm';
 
     const handleClose = () => setShow(false);
-    const updateEvent = (e) => setEvent(e.target.value);
+    const updateEvent = (e) => setEvent(e.target.value)
 
     const getCalendar = () => {
         fetch('/api/getCalendar/' + "k@f.com/" + value.format("YYYY-MM-DD"), {
@@ -90,28 +96,42 @@ function CalendarComponent () {
             </div>
 
             <Modal
-                show={show}
-                onHide={handleClose}
-                backdrop="static"
-                keyboard={false}
+                title={value.format("YYYY-MM-DD")}
+                style={{ top: 20 }}
+                visible={show}
+                onOk={() => updateCalendar()}
+                onCancel={() => setShow(!show)}
+                wrapClassName="vertical-center-modal"
             >
-                <Modal.Header closeButton>
-                    <Modal.Title>{value.format("YYYY-MM-DD")}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {currentEvents.length > 0 && currentEvents.map((item) =>
-                    <ListGroup.Item key={item.id} id = {item.id} action>
-                        {item.information}
-                        <CloseOutlined onClick={() => removeEvent(item.id)} style={{fontSize: '15px', color: 'red'}}/>
-                    </ListGroup.Item>
-                    )}
-                </Modal.Body>
-                <Form.Control className="input-event" size="sm" type="text" placeholder="Event" ref={inputRef} onChange={updateEvent}/>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>Close</Button>
-                    <Button variant="primary" onClick={updateCalendar}>Add</Button>
-                </Modal.Footer>
+                <hr/>
+                {/*{currentEvents.length > 0 && currentEvents.map((item) =>*/}
+                {/*        <ListGroup.Item key={item.id} id = {item.id} action>*/}
+                {/*            {item.information}*/}
+                {/*            <CloseOutlined onClick={() => removeEvent(item.id)} style={{fontSize: '15px', color: 'red'}}/>*/}
+                {/*        </ListGroup.Item>*/}
+                {/*        )}*/}
+                <TimePicker defaultValue={moment('12:00', format)} format={format} />
+                <input className="popup-input" type="text" id="event" size="30" onChange={updateEvent} ref={inputRef}/>
             </Modal>
+
+                {/*<Modal*/}
+                {/*    show={show}*/}
+                {/*    onHide={handleClose}*/}
+                {/*    backdrop="static"*/}
+                {/*    keyboard={false}*/}
+                {/*>*/}
+                {/*    <Modal.Header closeButton>*/}
+                {/*        <Modal.Title>{value.format("YYYY-MM-DD")}</Modal.Title>*/}
+                {/*    </Modal.Header>*/}
+                {/*    <Modal.Body>*/}
+                {/*
+                {/*    </Modal.Body>*/}
+                {/*    <Form.Control className="input-event" size="sm" type="text" placeholder="Event" ref={inputRef} onChange={updateEvent}/>*/}
+                {/*    <Modal.Footer>*/}
+                {/*        <Button variant="secondary" onClick={handleClose}>Close</Button>*/}
+                {/*        <Button variant="primary" onClick={updateCalendar}>Add</Button>*/}
+                {/*    </Modal.Footer>*/}
+                {/*</Modal>*/}
         </div>
     )
 }
